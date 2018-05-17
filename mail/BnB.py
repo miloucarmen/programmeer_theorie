fe8eeb90596681036da88415eb512ef672be7df3
Max Bijkerk <maxbijkerk@gmail.com>
2018-05-17 09:51:14 +0000

    printjes

--- mail/BnB.py
+++ mail/BnB.py
@@ -10,7 +10,7 @@ def randomMutation(testLength):
 current = [0] * 32
 best = []
 #melanoGenome = [0,23,1,2,11,24,22,19,6,10,7,25,20,5,8,18,12,13,14,15,16,17,21,3,4,9,26]
-melanoGenome = randomMutation(5)
+melanoGenome = randomMutation(7)
 
 genomeObject = helpersSteven.GenomeSequence(melanoGenome)
 mirandaGenome = [i for i in range(len(melanoGenome))]
@@ -22,88 +22,83 @@ mirandaGenome = [i for i in range(len(melanoGenome))]
 # upperBound vanaf 0 en dan omhoog?
 
 def BnB(genomeObj, depth, upperBound, current, best, breakpointPairs):
-##print("Branch and Bound was started!")
+    print("----------Branch and Bound was started-----------")
 
-    #print("Depth: ", depth)
-    #print("upperBound: ", upperBound)
-    #print("Current: ", current)
-    #print("Best: ", best)
+    print("Depth: ", depth)
+    print("upperBound: ", upperBound)
+    print("Current: ", current)
+    print("Best: ", best)
 
-    #print("Our current genome looks like this: ", genomeObj.genome)
+    print("Our current genome looks like this: ", genomeObj.genome)
     if genomeObj.genome == mirandaGenome:
 
-        #print("----------------------------!The genome has been SOLVED!----------------------------")
-        #print("Depth: ", depth)
-        #print("upperBound: ", upperBound)
-        #print("Current: ", current)
-        #print("Best: ", best)
+        print("--------------------------------------!The genome has been SOLVED!--------------------------------------")
 
         if depth <= upperBound:
+
+            print("!!A new best has been found!!")
+
             best = []
             upperBound, best = depth, current
+            print("Updated upperBound: ", upperBound)
+            print("Updated best: ", best)
 
-            # print("Depth: ", depth)
-            # print("upperBound: ", upperBound)
-            # print("Current: ", current)
-            # print("Best: ", best)
+            print("Return: upperbound, current, best")
 
             best = copy.copy(best)
             return upperBound, current, best
 
 
     else:
-        #print("optionList: ", genomeObj.Mutate("B&B"))
-        # print("breakpoint pairs", breakpointPairs)
-        # print("genome", genomeObj.genome)
+        print("The genome has not been solved yet.")
+
         allOptions = genomeObj.Mutate("B&B")
-        # print(allOptions)
         prevPoints = 0
+
+        print("The optionList equals: ", allOptions)
+
         for optionList in range(len(allOptions)):
+
             breakpointPairsCurrent = breakpointPairs - abs(optionList - 2)
             lowerBound = helpersSteven.LowerBound(breakpointPairsCurrent) + depth + 1
+            print("lowerBound is set to: ", lowerBound)
+            print("upperBound was already equal to: ", upperBound)
+            print("lowerBound <= upperBound = ", lowerBound <= upperBound)
 
             for option in allOptions[optionList]:
-                #print("option: ", option)
-                #print("lowerBound is set to: ", lowerBound)
-                #print("upperBound was already equal to: ", upperBound)
+
+                print("Currently revising from optionList ", optionList, ", ", option)
 
                 # check if lowerBound is less OR equal than upperBound.
                 # (we use leq, because the challenge is: if there are MULTIPLE best solutions, compare them)
                 if lowerBound <= upperBound:
                     i, j = option[0], option[1]
-                    #print("i equals: ", i)
-                    #print("j equals: ", j)
+                    print("i equals: ", i)
+                    print("j equals: ", j)
 
-                    #print("Depth: ", depth)
-                    #print("upperBound: ", upperBound)
-                    #print("Current: ", current)
-                    #print("Best: ", best)
                     current[depth] = (i, j)
 
-                    #print("After editing current[depth], current now equals: ", current)
+                    print("After editing current[depth], current now equals: ", current)
 
                     genomeObj.genome = genomeObj.Reverse(i, j)
 
-                    #print("After reversing the genome now equals: ", genomeObj.genome)
+                    print("After reversing the genome now equals: ", genomeObj.genome)
 
                     genomeObj.UpdateBreakpointList(i, j)
 
-                    print("Our breakpointlist is updated and equals: ", breakpointPairsCurrent)
+                    print("We go deeeeeeeepeeeer")
+
+                    upperBound, current, best = BnB(genomeObj, depth + 1, upperBound, current, best, breakpointPairsCurrent)
+
+                    print("We're out of depth")
+
+                    genomeObj.genome = genomeObj.Reverse(i,j)
+                    genomeObj.UpdateBreakpointList(i, j)
                     print("Our current genome is: einde ", genomeObj.genome)
                     print("Depth: ", depth)
                     print("upperBound: ", upperBound)
                     print("Current: ", current)
                     print("Best: ", best)
-                    print(genomeObj)
-                    upperBound, current, best = BnB(genomeObj, depth + 1, upperBound, current, best, breakpointPairsCurrent)
-
-                    genomeObj.genome = genomeObj.Reverse(i,j)
-                    genomeObj.UpdateBreakpointList(i, j)
-                    #print("Our current genome is: einde ", genomeObj.genome)
-                    #print("Depth: ", depth)
-                    #print("upperBound: ", upperBound)
-                    #print("Current: ", current)
-                    #print("Best: ", best)
                 else:
                     break
         return upperBound, current, best
